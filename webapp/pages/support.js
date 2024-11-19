752c5d3fdb31a06f9af99d3dc4f1727afb7123c0
nshahenlabel <nshah@enlabel.com>
2024-11-19 11:34:05 -0500

    support page from ui fixed

--- webapp/pages/support.js
+++ webapp/pages/support.js
@@ -14,71 +14,150 @@ import {
 } from '../src/app/components/muiComponents';
 import './support.css';
 import { useEdrawData } from '../src/app/utils/EdrawDataContext';
-
+import { useState } from 'react';
 
 export default function Support() {
     const supportPageData = useEdrawData("/api/Support-page");
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        contactNumber: '',
+        email: '',
+        companyName: '',
+        message: ''
+    });
+
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Submitted:", formData);
+        setFormSubmitted(true);
+    };
+
     if (!supportPageData) {
         return <Typography>Loading...</Typography>;
     }
+
     const { Title: edrawTitle, Description, WhyeDraw, WhyeDrawDesc, WhyeDrawSubHeading, Feature1 } = supportPageData;
-    console.log("test");
+
     return (
         <>
             <Container className='container'>
                 <Grid container className="grey-card">
-                    <Grid size={6}>
-                        <h2>Contact our team</h2>
+                    <Grid container  size={6} rowSpacing={6} columnSpacing={2} >
+                        <Grid >
+                        <h2 className='heading'>Contact our team</h2>
                         <p>
-                            We can’t wait to hear from you. Your input is important to us.0
+                            We can’t wait to hear from you. Your input is important to us.
                         </p>
-                        <img src="/vector15.png" alt="Vector 2" />
-                        <p>
+                        </Grid>
+                        <Grid className='support-img1'>
+                        <img  src="/vector15.png" alt="Vector 2" />
+                        </Grid>
+                        <Grid>
+                        <div>
                             {Description}
-                        </p>
+                        </div>
                         <hr className='hr'></hr>
                         <div>Reach out and we’ll get in touch within 24 hours</div>
+                        </Grid>
                     </Grid>
                     <Grid size={6}>
                         <Box className="support-form">
-                            <Grid container rowSpacing={4} columnSpacing={2}>
-                                <Grid size={6}>
-                                    <div>First Name</div>
-                                    <TextField fullWidth  placeholder='First Name' size="small" />
+                            <form onSubmit={handleSubmit}>
+                                <Grid container rowSpacing={1} columnSpacing={2} className="support-form">
+                                    <Grid size={6}>
+                                        <div className='text'>First Name *</div>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            placeholder='First Name'
+                                            size="small"
+                                            name="firstName"
+                                            value={formData.firstName}
+                                            onChange={handleChange}
+                                        />
+                                    </Grid>
+                                    <Grid size={6}>
+                                        <div>Last Name *</div>
+                                        <TextField
+                                            fullWidth
+                                            required
+                                            placeholder='Last Name'
+                                            size="small"
+                                            name="lastName"
+                                            value={formData.lastName}
+                                            onChange={handleChange}
+                                        />
+                                    </Grid>
+                                    <Grid size={12}>
+                                        <div>Contact Number *</div>
+                                        <TextField
+                                            fullWidth
+                                            required
+                                            placeholder='Enter Contact Number'
+                                            size="small"
+                                            name="contactNumber"
+                                            value={formData.contactNumber}
+                                            onChange={handleChange}
+                                        />
+                                    </Grid>
+                                    <Grid size={12}>
+                                        <div>Email ID *</div>
+                                        <TextField
+                                            fullWidth
+                                            required
+                                            placeholder='Enter Email ID'
+                                            size="small"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                        />
+                                    </Grid>
+                                    <Grid size={12}>
+                                        <div>Company Name *</div>
+                                        <TextField
+                                            fullWidth
+                                            required
+                                            placeholder='Enter Company Name '
+                                            size="small"
+                                            name="companyName"
+                                            value={formData.companyName}
+                                            onChange={handleChange}
+                                        />
+                                    </Grid>
+                                    <Grid size={12}>
+                                        <div>Message *</div>
+                                        <TextField
+                                            fullWidth
+                                            required
+                                            size="small"
+                                            name="message"
+                                            value={formData.message}
+                                            onChange={handleChange}
+                                        />
+                                    </Grid>
+                                    <Grid size={12}>
+                                        <Button type="submit" className="submitbtn">Submit</Button>
+                                    </Grid>
+                                    <Grid size={12}></Grid>
                                 </Grid>
-                                <Grid size={6}>
-                                <div>Last Name</div>
-                                    <TextField fullWidth  placeholder='Last  Name' size="small" />
-                                </Grid>
-                                <Grid size={12}>
-                                <div>Contact Number</div>
-                                <TextField fullWidth  placeholder='Enter Contact Number' size="small" />
-                                </Grid>
-                                <Grid size={12}>
-                                <div> Email ID</div>
-                                <TextField fullWidth  placeholder='Enter Email ID' size="small" />
-                                </Grid>
-                                <Grid size={12}>
-                                <div>Company Name</div>
-                                <TextField fullWidth  placeholder='Enter Company Name ' size="small" />
-                                </Grid>
-                                <Grid size={12}>
-                                <div>Message</div>
-                                <TextField fullWidth size="small" />
-                                </Grid>
-                                <Grid size={12}>
-                                <Button className="submitbtn">Submit</Button>
-                                </Grid>
-                                <Grid size={12}>
-
-                                </Grid>
-                            </Grid>
+                            </form>
+                            {formSubmitted && (
+                                <Typography>Thank you for contacting us! We'll get back to you soon.</Typography>
+                            )}
                         </Box>
-                        <img className='support-img' src='/vector17.png'/>
+                        <img className='support-img' src='/vector17.png' />
                     </Grid>
                 </Grid>
             </Container>
-
         </>
     );
 }
