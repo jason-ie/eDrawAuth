d3217068f6d6ac3ee1887925c4683c174b93721f
nshahenlabel <nshah@enlabel.com>
2024-12-03 15:05:05 -0500

    suppot form css changed to tailwind

--- webapp/pages/support.js
+++ webapp/pages/support.js
@@ -68,139 +68,143 @@ export default function Support() {
     // }
 
     // const { Title: edrawTitle, Description, WhyeDraw, WhyeDrawDesc, WhyeDrawSubHeading, Feature1 } = supportPageData;
-
+    const inputclassname = " required w-full py-1.5 pl-1 placeholder:text-gray-400 sm:text-sm/6  border-gray-200 rounded-[5px] focus:border-white focus:outline-none focus:ring-2 focus:border-gray-400"
     return (
         <>
-            <Container className="container">
-                <Grid container className="grey-card">
-                    <Grid
-                        size={{ xs: 12, sm: 12, md: 6, lg: 6, xl: 6 }}
-                        rowSpacing={6}
-                        columnSpacing={2}
-                    >
-                        <Grid>
+            <div className='container'>
+                <div className='grey-card'>
+                    <div className='grid grid-cols-12'>
+                        <div className='col-span-6'>
                             <h1 className="heading">Contact our team</h1>
                             <p>
                                 We can’t wait to hear from you. Your input is
                                 important to us.
                             </p>
-                        </Grid>
-                        <Grid className="support-img1">
                             <img src="/vector15.png" alt="Vector 2" />
-                        </Grid>
-                        <Grid>
-                            <div>{/* {Description} */}</div>
                             <hr className="hr"></hr>
                             <div>
                                 Reach out and we’ll get in touch within 24 hours
                             </div>
-                        </Grid>
-                    </Grid>
-                    <Grid size={{ xs: 12, sm: 12, md: 6, lg: 6, xl: 6 }}>
-                        <Box className="support-form">
+                        </div>
+
+                        <div className='col-span-6 support-form'>
                             <form onSubmit={handleSubmit}>
-                                <Grid
-                                    container
-                                    rowSpacing={1}
-                                    columnSpacing={2}
-                                    className="support-form"
+                                <div className=' grid grid-cols-12'>
+                                    <div className='col-span-6'>
+                                        <label htmlFor="firstname">
+                                            First Name *
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+
+                                                type="text"
+                                                placeholder="First Name"
+                                                className={inputclassname}
+                                                value={formData.firstName}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='col-span-6'>
+                                        <label htmlFor="price">
+                                            Last name *
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="lastname"
+                                                name="lastname"
+                                                type="text"
+                                                placeholder="Last name *"
+                                                className={inputclassname}
+                                                value={formData.lastName}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='col-span-12'>
+                                        <label htmlFor="price">
+                                            Contact Number *
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="contactnumber"
+                                                name="contactnumber"
+                                                type="text"
+                                                placeholder="Enter Contact Number"
+                                                className={inputclassname}
+                                                value={formData.contactNumber}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='col-span-12'>
+                                        <label htmlFor="price">
+                                            Email ID *
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="emailid"
+                                                name="emailid"
+                                                type="text"
+                                                placeholder="Enter Email ID "
+                                                className={inputclassname}
+                                                value={formData.email}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='col-span-12'>
+                                        <label htmlFor="price">
+                                            Company Name *
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="companyname"
+                                                name="companyname"
+                                                type="text"
+                                                placeholder="Enter Compnay Name"
+                                                className={inputclassname}
+                                                value={formData.companyName}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className='col-span-12'>
+                                        <label htmlFor="price">
+                                            Message
+                                        </label>
+                                        <div className="mt-2">
+                                            <input
+                                                id="message"
+                                                name="message"
+                                                type="text"
+                                                placeholder="Message"
+                                                className={inputclassname}
+                                                value={formData.message}
+                                                onChange={handleChange}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <Button
+                                    type="submit"
+                                    className="submitbtn"
+                                    disabled={isLoading}
                                 >
-                                    <Grid size={6}>
-                                        <div className="text">First Name *</div>
-                                        <TextField
-                                            required
-                                            fullWidth
-                                            placeholder="First Name"
-                                            size="small"
-                                            name="firstName"
-                                            value={formData.firstName}
-                                            onChange={handleChange}
-                                        />
-                                    </Grid>
-                                    <Grid size={6}>
-                                        <div>Last Name *</div>
-                                        <TextField
-                                            fullWidth
-                                            required
-                                            placeholder="Last Name"
-                                            size="small"
-                                            name="lastName"
-                                            value={formData.lastName}
-                                            onChange={handleChange}
-                                        />
-                                    </Grid>
-                                    <Grid size={12}>
-                                        <div>Contact Number *</div>
-                                        <TextField
-                                            fullWidth
-                                            required
-                                            placeholder="Enter Contact Number"
-                                            size="small"
-                                            name="contactNumber"
-                                            value={formData.contactNumber}
-                                            onChange={handleChange}
-                                        />
-                                    </Grid>
-                                    <Grid size={12}>
-                                        <div>Email ID *</div>
-                                        <TextField
-                                            fullWidth
-                                            required
-                                            placeholder="Enter Email ID"
-                                            size="small"
-                                            name="email"
-                                            value={formData.email}
-                                            onChange={handleChange}
-                                        />
-                                    </Grid>
-                                    <Grid size={12}>
-                                        <div>Company Name *</div>
-                                        <TextField
-                                            fullWidth
-                                            required
-                                            placeholder="Enter Company Name "
-                                            size="small"
-                                            name="companyName"
-                                            value={formData.companyName}
-                                            onChange={handleChange}
-                                        />
-                                    </Grid>
-                                    <Grid size={12}>
-                                        <div>Message *</div>
-                                        <TextField
-                                            fullWidth
-                                            required
-                                            size="small"
-                                            name="message"
-                                            value={formData.message}
-                                            onChange={handleChange}
-                                        />
-                                    </Grid>
-                                    <Grid size={12}>
-                                        <Button
-                                            type="submit"
-                                            className="submitbtn"
-                                            disabled={isLoading}
-                                        >
-                                            {isLoading
-                                                ? 'Sending...'
-                                                : 'Submit'}
-                                        </Button>
-                                    </Grid>
-                                    <Grid size={12}></Grid>
-                                </Grid>
+                                    {isLoading
+                                        ? 'Sending...'
+                                        : 'Submit'}
+                                </Button>
                             </form>
-                            {formSubmitted && (
-                                <Typography>
-                                    Thank you for contacting us! We'll get back
-                                    to you soon.
-                                </Typography>
-                            )}
-                        </Box>
-                        <img className="support-img" src="/vector17.png" />
-                    </Grid>
-                </Grid>
-            </Container>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
         </>
     );
 }
